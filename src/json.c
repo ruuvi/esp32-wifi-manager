ec9904e7505fde07b754ca1283b240c8cff69ede
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-08-23 02:14:55 +0700

    #30: Fix vulnerability "Remove the use of this insecure "sprintf" function": json.c refactoring

--- src/json.c
+++ src/json.c
@@ -2,7 +2,8 @@
 @file json.c
 @brief handles very basic JSON with a minimal footprint on the system
 
-This code is a lightly modified version of cJSON 1.4.7. cJSON is licensed under the MIT license:
+This code is a completely rewritten version of cJSON 1.4.7.
+cJSON is licensed under the MIT license:
 Copyright (c) 2009 Dave Gamble
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,114 +30,94 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdarg.h>
 #include "json.h"
 
+__attribute__((format(printf, 4, 5))) //
 bool
-json_print_string(const unsigned char *input, unsigned char *output_buffer)
+json_snprintf(int32_t *pIdx, char *buf, const size_t buf_size, char *fmt, ...)
 {
-    const unsigned char *input_pointer  = NULL;
-    unsigned char *      output         = NULL;
-    unsigned char *      output_pointer = NULL;
-    size_t               output_length  = 0;
-    /* numbers of additional characters needed for escaping */
-    size_t escape_characters = 0;
+    const int32_t idx = *pIdx;
+    if (idx < 0)
+    {
+        return false;
+    }
+    va_list args;
+    va_start(args, fmt);
+    const int len = vsnprintf(&buf[idx], buf_size - idx, fmt, args);
+    va_end(args);
+    if (len < 0)
+    {
+        *pIdx = -1;
+        return false;
+    }
+    *pIdx += len;
+    if (*pIdx >= buf_size)
+    {
+        *pIdx = -1;
+        return false;
+    }
+    return true;
+}
 
-    if (output_buffer == NULL)
+bool
+json_print_escaped_string(int32_t *p_out_buf_idx, char *p_out_buf, const size_t out_buf_size, const char *p_input_str)
+{
+    if (NULL == p_out_buf)
     {
         return false;
     }
 
-    /* empty string */
-    if (input == NULL)
+    if (NULL == p_input_str)
     {
-        // output = ensure(output_buffer, sizeof("\"\""), hooks);
-        if (output == NULL)
+        if (!json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\"\""))
         {
             return false;
         }
-        strcpy((char *)output, "\"\"");
-
         return true;
     }
 
-    /* set "flag" to 1 if something needs to be escaped */
-    for (input_pointer = input; *input_pointer; input_pointer++)
+    json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\"");
+    for (const char *in_ptr = p_input_str; '\0' != *in_ptr; ++in_ptr)
     {
-        if (strchr("\"\\\b\f\n\r\t", *input_pointer))
+        const char in_chr = *in_ptr;
+        switch (in_chr)
         {
-            /* one character escape sequence */
-            escape_characters++;
-        }
-        else if (*input_pointer < 32)
-        {
-            /* UTF-16 escape sequence uXXXX */
-            escape_characters += 5;
+            case '\\':
+            case '\"':
+                json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\\%c", in_chr);
+                break;
+            case '\b':
+                json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\\b");
+                break;
+            case '\f':
+                json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\\f");
+                break;
+            case '\n':
+                json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\\n");
+                break;
+            case '\r':
+                json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\\r");
+                break;
+            case '\t':
+                json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\\t");
+                break;
+            default:
+                if (in_chr >= '\x20')
+                {
+                    /* normal character, copy */
+                    json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "%c", in_chr);
+                }
+                else
+                {
+                    json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\\u%04x", in_chr);
+                }
+                break;
         }
     }
-    output_length = (size_t)(input_pointer - input) + escape_characters;
-
-    /* in the original cJSON it is possible to realloc here in case output buffer is too small.
-     * This is overkill for an embedded system. */
-    output = output_buffer;
-
-    /* no characters have to be escaped */
-    if (escape_characters == 0)
+    if (!json_snprintf(p_out_buf_idx, p_out_buf, out_buf_size, "\""))
     {
-        output[0] = '\"';
-        memcpy(output + 1, input, output_length);
-        output[output_length + 1] = '\"';
-        output[output_length + 2] = '\0';
-
-        return true;
+        return false;
     }
-
-    output[0]      = '\"';
-    output_pointer = output + 1;
-    /* copy the string */
-    for (input_pointer = input; *input_pointer != '\0'; (void)input_pointer++, output_pointer++)
-    {
-        if ((*input_pointer > 31) && (*input_pointer != '\"') && (*input_pointer != '\\'))
-        {
-            /* normal character, copy */
-            *output_pointer = *input_pointer;
-        }
-        else
-        {
-            /* character needs to be escaped */
-            *output_pointer++ = '\\';
-            switch (*input_pointer)
-            {
-                case '\\':
-                    *output_pointer = '\\';
-                    break;
-                case '\"':
-                    *output_pointer = '\"';
-                    break;
-                case '\b':
-                    *output_pointer = 'b';
-                    break;
-                case '\f':
-                    *output_pointer = 'f';
-                    break;
-                case '\n':
-                    *output_pointer = 'n';
-                    break;
-                case '\r':
-                    *output_pointer = 'r';
-                    break;
-                case '\t':
-                    *output_pointer = 't';
-                    break;
-                default:
-                    /* escape and print as unicode codepoint */
-                    sprintf((char *)output_pointer, "u%04x", *input_pointer);
-                    output_pointer += 4;
-                    break;
-            }
-        }
-    }
-    output[output_length + 1] = '\"';
-    output[output_length + 2] = '\0';
-
     return true;
 }
--- src/json.h
+++ src/json.h
@@ -2,7 +2,8 @@
 @file json.h
 @brief handles very basic JSON with a minimal footprint on the system
 
-This code is a lightly modified version of cJSON 1.4.7. cJSON is licensed under the MIT license:
+This code is a completely rewritten version of cJSON 1.4.7.
+cJSON is licensed under the MIT license:
 Copyright (c) 2009 Dave Gamble
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,19 +29,29 @@ OTHER DEALINGS IN THE SOFTWARE.
 #ifndef JSON_H_INCLUDED
 #define JSON_H_INCLUDED
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+__attribute__((format(printf, 4, 5))) //
+bool
+json_snprintf(int32_t *pIdx, char *buf, const size_t buf_size, char *fmt, ...);
+
 /**
  * @brief Render the cstring provided to a JSON escaped version that can be printed.
- * @param input the input buffer to be escaped.
- * @param output_buffer the output buffer to write to. You must ensure it is big enough to contain the final string.
- * @see cJSON equivlaent static cJSON_bool print_string_ptr(const unsigned char * const input, printbuffer * const
+ * @param p_out_buf_idx - a pointer to a variable that contains the current offset in the output buffer p_out_buf.
+ * @param p_out_buf the output buffer to write to.
+ * @param out_buf_size the length of the output buffer.
+ * @param p_input_str the p_input_str buffer to be escaped.
+ * @see cJSON equivalent static cJSON_bool print_string_ptr(const unsigned char * const p_input_str, printbuffer * const
  * output_buffer)
  */
 bool
-json_print_string(const unsigned char *input, unsigned char *output_buffer);
+json_print_escaped_string(int32_t *p_out_buf_idx, char *p_out_buf, const size_t out_buf_size, const char *p_input_str);
 
 #ifdef __cplusplus
 }
