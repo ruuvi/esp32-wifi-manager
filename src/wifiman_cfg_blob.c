dab3cd0acef52fbb2df6a8a7ca601fab9222ee52
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2022-04-24 20:31:39 +0300

    #164: Add unit-tests for wifi_cfg_blob conversion

--- src/wifiman_cfg_blob.c
+++ src/wifiman_cfg_blob.c
@@ -9,9 +9,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include "nvs.h"
-#include "esp_wifi_types.h"
 #include "wifi_manager_defs.h"
-#include "wifiman_config.h"
+#include "wifiman_cfg_blob_convert.h"
 
 #define LOG_LOCAL_LEVEL LOG_LEVEL_INFO
 #include "log.h"
@@ -19,32 +18,6 @@
 #if LOG_LOCAL_LEVEL >= LOG_LEVEL_DEBUG
 #warning Debug log level prints out the passwords as a "plaintext".
 #endif
-
-#define WIFIMAN_CFG_BLOB_MAX_SSID_SIZE     (32U)
-#define WIFIMAN_CFG_BLOB_MAX_PASSWORD_SIZE (64U)
-
-typedef struct wifiman_cfg_blob_wifi_ssid_t
-{
-    char ssid_buf[WIFIMAN_CFG_BLOB_MAX_SSID_SIZE];
-} wifiman_cfg_blob_wifi_ssid_t;
-
-typedef struct wifiman_cfg_blob_wifi_password_t
-{
-    char password_buf[WIFIMAN_CFG_BLOB_MAX_PASSWORD_SIZE];
-} wifiman_cfg_blob_wifi_password_t;
-
-typedef struct wifiman_cfg_blob_wifi_settings_t
-{
-    uint8_t             ap_ssid[WIFIMAN_CFG_BLOB_MAX_SSID_SIZE];
-    uint8_t             ap_pwd[WIFIMAN_CFG_BLOB_MAX_PASSWORD_SIZE];
-    uint8_t             ap_channel;
-    uint8_t             ap_ssid_hidden;
-    wifi_bandwidth_t    ap_bandwidth;
-    bool                sta_only;
-    wifi_ps_type_t      sta_power_save;
-    bool                sta_static_ip;
-    esp_netif_ip_info_t sta_static_ip_config;
-} wifiman_cfg_blob_wifi_settings_t;
 
 static const char TAG[] = "wifi_manager";
 
@@ -87,21 +60,17 @@ wifiman_config_nvs_get_blob(const nvs_handle_t handle, const char *const key, vo
 }
 
 static bool
-wifiman_cfg_blob_read_by_handle(
-    const nvs_handle                        handle,
-    wifiman_cfg_blob_wifi_ssid_t *const     p_ssid,
-    wifiman_cfg_blob_wifi_password_t *const p_password,
-    wifiman_cfg_blob_wifi_settings_t *const p_wifi_settings)
+wifiman_cfg_blob_read_by_handle(const nvs_handle handle, wifiman_cfg_blob_t *const p_blob)
 {
-    if (!wifiman_config_nvs_get_blob(handle, "ssid", p_ssid->ssid_buf, MAX_SSID_SIZE))
+    if (!wifiman_config_nvs_get_blob(handle, "ssid", p_blob->sta_ssid.ssid_buf, MAX_SSID_SIZE))
     {
         return false;
     }
-    if (!wifiman_config_nvs_get_blob(handle, "password", p_password->password_buf, MAX_PASSWORD_SIZE))
+    if (!wifiman_config_nvs_get_blob(handle, "password", p_blob->sta_password.password_buf, MAX_PASSWORD_SIZE))
     {
         return false;
     }
-    if (!wifiman_config_nvs_get_blob(handle, "settings", p_wifi_settings, sizeof(*p_wifi_settings)))
+    if (!wifiman_config_nvs_get_blob(handle, "settings", &p_blob->wifi_settings, sizeof(p_blob->wifi_settings)))
     {
         return false;
     }
@@ -118,10 +87,8 @@ wifi_manager_cfg_blob_read(wifiman_config_t *const p_cfg)
         return false;
     }
 
-    wifiman_cfg_blob_wifi_ssid_t     sta_ssid      = { 0 };
-    wifiman_cfg_blob_wifi_password_t sta_password  = { 0 };
-    wifiman_cfg_blob_wifi_settings_t wifi_settings = { 0 };
-    const bool res = wifiman_cfg_blob_read_by_handle(handle, &sta_ssid, &sta_password, &wifi_settings);
+    wifiman_cfg_blob_t cfg_blob = { 0 };
+    const bool         res      = wifiman_cfg_blob_read_by_handle(handle, &cfg_blob);
 
     nvs_close(handle);
 
@@ -131,47 +98,7 @@ wifi_manager_cfg_blob_read(wifiman_config_t *const p_cfg)
         return false;
     }
 
-    *p_cfg = *wifiman_default_config_get();
-
-    p_cfg->wifi_config_ap.channel     = wifi_settings.ap_channel;
-    p_cfg->wifi_config_ap.ssid_hidden = (0 != wifi_settings.ap_ssid_hidden) ? 1 : 0;
-
-    switch (wifi_settings.ap_bandwidth)
-    {
-        case WIFI_BW_HT20:
-        case WIFI_BW_HT40:
-            p_cfg->wifi_settings_ap.ap_bandwidth = wifi_settings.ap_bandwidth;
-            break;
-        default:
-            LOG_WARN("%s: Unknown ap_bandwidth=%d, force set to WIFI_BW_HT20", __func__, wifi_settings.ap_bandwidth);
-            p_cfg->wifi_settings_ap.ap_bandwidth = WIFI_BW_HT20;
-            break;
-    }
-
-    (void)snprintf((char *)p_cfg->wifi_config_sta.ssid, sizeof(p_cfg->wifi_config_sta.ssid), "%s", sta_ssid.ssid_buf);
-    (void)snprintf(
-        (char *)p_cfg->wifi_config_sta.password,
-        sizeof(p_cfg->wifi_config_sta.password),
-        "%s",
-        sta_password.password_buf);
-
-    switch (wifi_settings.sta_power_save)
-    {
-        case WIFI_PS_NONE:
-        case WIFI_PS_MIN_MODEM:
-        case WIFI_PS_MAX_MODEM:
-            p_cfg->wifi_settings_sta.sta_power_save = wifi_settings.sta_power_save;
-            break;
-        default:
-            LOG_WARN(
-                "%s: Unknown sta_power_save=%d, force set to WIFI_PS_NONE",
-                __func__,
-                wifi_settings.sta_power_save);
-            p_cfg->wifi_settings_sta.sta_power_save = WIFI_PS_NONE;
-            break;
-    }
-    p_cfg->wifi_settings_sta.sta_static_ip        = !!wifi_settings.sta_static_ip;
-    p_cfg->wifi_settings_sta.sta_static_ip_config = wifi_settings.sta_static_ip_config;
+    wifiman_cfg_blob_convert(&cfg_blob, p_cfg);
 
     return true;
 }
---
+++ src/wifiman_cfg_blob_convert.c
@@ -0,0 +1,81 @@
+/**
+ * @file wifiman_cfg_blob_convert.c
+ * @author TheSomeMan
+ * @date 2022-04-24
+ * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
+ */
+
+#include "wifiman_cfg_blob_convert.h"
+#include "wifiman_config.h"
+
+#define LOG_LOCAL_LEVEL LOG_LEVEL_INFO
+#include "log.h"
+
+#if LOG_LOCAL_LEVEL >= LOG_LEVEL_DEBUG
+#warning Debug log level prints out the passwords as a "plaintext".
+#endif
+
+static const char TAG[] = "wifi_manager";
+
+void
+wifiman_cfg_blob_convert(const wifiman_cfg_blob_t *const p_cfg_blob_src, wifiman_config_t *const p_cfg_dst)
+{
+    const wifiman_config_t *const p_cfg_default = wifiman_default_config_get();
+
+    *p_cfg_dst = *p_cfg_default;
+
+    if (p_cfg_blob_src->wifi_settings.ap_channel <= 14)
+    {
+        p_cfg_dst->wifi_config_ap.channel = p_cfg_blob_src->wifi_settings.ap_channel;
+    }
+    else
+    {
+        LOG_WARN("%s: Unknown ap_channel=%d, force set to 0", __func__, p_cfg_blob_src->wifi_settings.ap_channel);
+        p_cfg_dst->wifi_config_ap.channel = 0;
+    }
+    p_cfg_dst->wifi_config_ap.ssid_hidden = (0 != p_cfg_blob_src->wifi_settings.ap_ssid_hidden) ? 1 : 0;
+
+    switch (p_cfg_blob_src->wifi_settings.ap_bandwidth)
+    {
+        case WIFI_BW_HT20:
+        case WIFI_BW_HT40:
+            p_cfg_dst->wifi_settings_ap.ap_bandwidth = p_cfg_blob_src->wifi_settings.ap_bandwidth;
+            break;
+        default:
+            LOG_WARN(
+                "%s: Unknown ap_bandwidth=%d, force set to WIFI_BW_HT20",
+                __func__,
+                p_cfg_blob_src->wifi_settings.ap_bandwidth);
+            p_cfg_dst->wifi_settings_ap.ap_bandwidth = p_cfg_default->wifi_settings_ap.ap_bandwidth;
+            break;
+    }
+
+    (void)snprintf(
+        (char *)p_cfg_dst->wifi_config_sta.ssid,
+        sizeof(p_cfg_dst->wifi_config_sta.ssid),
+        "%s",
+        p_cfg_blob_src->sta_ssid.ssid_buf);
+    (void)snprintf(
+        (char *)p_cfg_dst->wifi_config_sta.password,
+        sizeof(p_cfg_dst->wifi_config_sta.password),
+        "%s",
+        p_cfg_blob_src->sta_password.password_buf);
+
+    switch (p_cfg_blob_src->wifi_settings.sta_power_save)
+    {
+        case WIFI_PS_NONE:
+        case WIFI_PS_MIN_MODEM:
+        case WIFI_PS_MAX_MODEM:
+            p_cfg_dst->wifi_settings_sta.sta_power_save = p_cfg_blob_src->wifi_settings.sta_power_save;
+            break;
+        default:
+            LOG_WARN(
+                "%s: Unknown sta_power_save=%d, force set to WIFI_PS_NONE",
+                __func__,
+                p_cfg_blob_src->wifi_settings.sta_power_save);
+            p_cfg_dst->wifi_settings_sta.sta_power_save = p_cfg_dst->wifi_settings_sta.sta_power_save;
+            break;
+    }
+    p_cfg_dst->wifi_settings_sta.sta_static_ip        = !!p_cfg_blob_src->wifi_settings.sta_static_ip;
+    p_cfg_dst->wifi_settings_sta.sta_static_ip_config = p_cfg_blob_src->wifi_settings.sta_static_ip_config;
+}
