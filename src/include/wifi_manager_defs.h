da3175c891f2f3fdf6a11593953d3494a4e119d9
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2023-01-18 13:43:47 +0700

    #228: Add handling of HTTP error 429 (Too Many Requests)

--- src/include/wifi_manager_defs.h
+++ src/include/wifi_manager_defs.h
@@ -226,6 +226,7 @@ typedef enum http_resp_code_e
     HTTP_RESP_CODE_401 = 401, // Unauthorized
     HTTP_RESP_CODE_403 = 403, // Forbidden
     HTTP_RESP_CODE_404 = 404, // Not Found
+    HTTP_RESP_CODE_429 = 429, // Too Many Requests
     HTTP_RESP_CODE_500 = 500, // Internal Server Error
     HTTP_RESP_CODE_502 = 502, // Bad Gateway
     HTTP_RESP_CODE_503 = 503, // Service Unavailable
